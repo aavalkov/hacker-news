64b35c7ee31dd0e8fb40674bc57e98446202887e
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-29 12:04:47 -0700

    edit and update comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,20 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment Updated"
+      redirect_to comment_path(@comment)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:name, :link_id)
