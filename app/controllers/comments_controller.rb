fa833d92b9585e2655168644767ffd2bf95d7dc7
Anna Valkov <anna.a.valkov@gmail.com>
2014-08-29 11:48:49 -0700

    show an individual comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,10 @@ class CommentsController < ApplicationController
     end
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:name, :link_id)
